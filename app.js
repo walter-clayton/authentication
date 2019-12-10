6b0bd8e10f26e84170e88d101e41664b2a7daeae
Walter Clayton <walthedude@gmail.com>
2019-12-10 10:05:04 +0100

    finished

--- app.js
+++ app.js
@@ -1,5 +1,11 @@
-var express = require("express");
-var mongoose = require("mongoose");
+var  express                = require("express"),
+     mongoose               = require("mongoose"),
+     passport               = require("passport"),
+     bodyParser             = require("body-parser"),
+     User                   = require("./models/user"),
+     LocalStrategy          = require("passport-local"),
+     passportLocalMongoose  = require("passport-local-mongoose")
+
 
 
 mongoose.connect('mongodb://localhost/auth_demo_app', {useNewUrlParser: true, useUnifiedTopology: true}, (err) => {
@@ -10,17 +16,95 @@ mongoose.connect('mongodb://localhost/auth_demo_app', {useNewUrlParser: true, us
     }
 });
 
+
+
 var app = express();
 
 app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(require("express-session")({
+    secret: "Rusty is the best !",
+    resave: false,
+    saveUninitialized: false
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+
+
+
+
+// ===================
+// ROUTES
+// ===================
+
+
+
 
 app.get("/", function(req, res){
     res.render("home");
 });
 
-app.get("/secret", function(req, res){
+app.get("/secret", isLoggedIn, function(req, res){
     res.render("secret");
 })
+
+
+
+// AUTH ROUTES
+
+// show form
+app.get("/register", function(req, res){
+    res.render("register");
+})
+
+// handling user sign up
+app.post("/register", function(req, res){
+    req.body.username
+    req.body.password
+    User.register(new User({username: req.body.username}), req.body.password, function(err, user) {
+        if(err) {
+            console.log(err);
+            return res.render("register");
+        }
+        passport.authenticate("local")(req, res, function(){
+            res.redirect("/secret");
+        })
+    })
+});
+
+
+// LOGIN ROUTES
+// render login form
+
+app.get("/login", function(req, res){
+    res.render("login");
+})
+
+// login logic
+app.post("/login", passport.authenticate("local", {
+    successRedirect: "/secret",
+    failureRedirect: "/login"
+}), function(req, res) {
+});
+
+// LOGOUT ROUTES
+app.get("/logout", function(req, res){
+    req.logout();
+    res.redirect("/");
+});
+
+function isLoggedIn(req, res, next){
+    if(req.isAuthenticated()){
+        return next();
+    }
+    res.redirect("/login");
+}
 
 var port = process.env.PORT || 3000;
 app.listen(3000, function(){
